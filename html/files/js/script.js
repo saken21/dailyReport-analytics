903adb14ce0061ac3ab8a2b54cf5b3638bfa9f62
Sakata <sakata@graphic.co.jp>
2015-09-01 14:53:05 +0900

    first commit.

---
+++ html/files/js/script.js
@@ -0,0 +1,10 @@
+(function () { "use strict";
+var js = {};
+var src = {};
+src.Main = function() { };
+src.Main.main = function() {
+};
+var q = window.jQuery;
+js.JQuery = q;
+src.Main.main();
+})();
---
+++ src/Main.hx
@@ -0,0 +1,29 @@
+/**
+* ================================================================================
+*
+* Daily Report Analytics ver 1.00.00
+*
+* Author : KENTA SAKATA
+* Since  : 2015/09/01
+* Update : 2015/09/01
+*
+* Licensed under the MIT License
+* Copyright (c) Kenta Sakata
+* http://saken.jp/
+*
+* ================================================================================
+*
+**/
+package src;
+
+import js.JQuery;
+
+class Main {
+	
+	public static function main():Void {
+		
+		//new JQuery('document').ready(Manager.init);
+		
+	}
+
+}
